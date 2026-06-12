e940ecd704144bdac16acdcdb0fd95f2b3460904
aalfonso526-ctrl <aalfonso526@gmail.com>
2026-06-12 17:18:49 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,13 +9,13 @@
    Debe quedar así (con TUS valores):
 
    var FIREBASE_CONFIG = {
-     apiKey: "AIzaSy...",
-     authDomain: "mis-tareas-xxxxx.firebaseapp.com",
-     projectId: "mis-tareas-xxxxx",
-     storageBucket: "mis-tareas-xxxxx.appspot.com",
-     messagingSenderId: "123456789",
-     appId: "1:123456789:web:abc123"
-   };
+  apiKey: "AIzaSyAR2HXEMA_XP4PuZaiZ2IWtyrF5Z4B_tbk",
+  authDomain: "mis-tareas-97b8d.firebaseapp.com",
+  projectId: "mis-tareas-97b8d",
+  storageBucket: "mis-tareas-97b8d.firebasestorage.app",
+  messagingSenderId: "342091244780",
+  appId: "1:342091244780:web:4942669d9f8177a8ab31c9"
+};
    ============================================================ */
 
 var FIREBASE_CONFIG = null;
